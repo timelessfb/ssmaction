7ed513ae43809c270141c7fe8e06d76a6807d60a
timelessfb <1282374749@qq.com>
2017-12-17 15:54:51 +0800

    12/17

--- src/main/java/edu/uestc/ssmdemo/web/CategoryController.java
+++ src/main/java/edu/uestc/ssmdemo/web/CategoryController.java
@@ -1,7 +1,10 @@
 package edu.uestc.ssmdemo.web;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import edu.uestc.ssmdemo.entity.Category;
 import edu.uestc.ssmdemo.service.CategoryService;
+import edu.uestc.ssmdemo.utils.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,9 +19,13 @@ public class CategoryController {
     CategoryService categoryService;
 
     @RequestMapping("admin_category_list")
-    public String list(Model model) {
+    public String list(Model model, Page page) {
+        PageHelper.offsetPage(page.getStart(), page.getCount());
         List<Category> cs = categoryService.list();
+        int total = (int) new PageInfo<Category>(cs).getTotal();
+        page.setTotal(total);
         model.addAttribute("cs", cs);
+        model.addAttribute("page", page);
         return "admin/listCategory";
     }
 
