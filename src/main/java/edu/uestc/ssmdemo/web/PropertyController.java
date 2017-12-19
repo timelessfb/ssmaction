55dffde5f756aec4552a73279fd3c5e1f5a53b68
timelessfb <1282374749@qq.com>
2017-12-19 22:21:10 +0800

    12/19

--- src/main/java/edu/uestc/ssmdemo/web/PropertyController.java
+++ src/main/java/edu/uestc/ssmdemo/web/PropertyController.java
@@ -1,9 +1,13 @@
 package edu.uestc.ssmdemo.web;
 
+import com.github.pagehelper.PageHelper;
+import edu.uestc.ssmdemo.entity.Category;
 import edu.uestc.ssmdemo.service.CategoryService;
 import edu.uestc.ssmdemo.service.PropertyService;
+import edu.uestc.ssmdemo.utils.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,4 +17,11 @@ public class PropertyController {
     CategoryService categoryService;
     @Autowired
     PropertyService propertyService;
+
+    @RequestMapping("admin_property_list")
+    public String list(int cid, Model model, Page page) {
+        Category c = categoryService.get(cid);
+        PageHelper.offsetPage(page.getStart(), page.getCount());
+        return super.toString();
+    }
 }
